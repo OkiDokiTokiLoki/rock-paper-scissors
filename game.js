5bc163d5d897b84f0259b2c420a70f81326907e1
tokiloki <codebadger1@gmail.com>
2021-12-07 16:58:45 +0200

    footer added

--- game.js
+++ game.js
@@ -55,13 +55,13 @@ function updateScore(){
 
     if (roundWinner === 'tie'){
         scoreInfo.style.color = 'white';
-        scoreInfo.textContent = "== it's a tie ==";
+        scoreInfo.textContent = "= it's a tie =";
     } else if (roundWinner === 'player'){
         scoreInfo.style.color = 'var(--orange)';
-        scoreInfo.textContent = '++ you win this round! ++';
+        scoreInfo.textContent = '+ you win this round! +';
     } else if (roundWinner === 'computer'){
         scoreInfo.style.color = 'var(--pink)';
-        scoreInfo.textContent = '-- computer wins this round --';
+        scoreInfo.textContent = '- computer wins this round -';
     }
 
     pTally.textContent = `${playerScore}`;
